312547ccc476cbe9d073a21b1abc68b4ea95f6df
Plp <yanick2019@yahoo.fr>
2019-07-05 12:51:52 +0200

    home page

---
+++ build/precache-manifest.c651fcd267793cdf08c3d41fed8a94ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2c2183a4d730528daccb4143d61b7b",
+    "url": "/ReactJS-tuto/index.html"
+  },
+  {
+    "revision": "f203706ce62dd8995556",
+    "url": "/ReactJS-tuto/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f203706ce62dd8995556",
+    "url": "/ReactJS-tuto/static/js/2.6b69e31a.chunk.js"
+  },
+  {
+    "revision": "8e0aedc6428f49bbfe3e",
+    "url": "/ReactJS-tuto/static/js/main.b9aa1e99.chunk.js"
+  },
+  {
+    "revision": "d01d6bb26a784833d75d",
+    "url": "/ReactJS-tuto/static/js/runtime~main.8ae1827e.js"
+  }
+]);
